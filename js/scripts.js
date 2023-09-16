60a847e7835f201ee7bd704540579de67b79f7e1
mrfukui <mr.fukui@yahoo.com>
2023-09-15 19:16:41 -0700

    created pokemonList array and 4 new objects

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+let pokemonList = [];
+pokemonList = [
+    {name: 'Bulbasaur',
+    type: ['Grass', 'Poison'],
+    height: 0.7
+    },
+    {name: 'Charmander',
+    type: ['Fire'],
+    height: 0.6
+    },
+    {name: 'Squirtle',
+    type: ['Water'],
+    height: 0.5
+    },
+    {name: 'Pikachu',
+    type: ['Electric'],
+    height: 0.4
+    }
+];
