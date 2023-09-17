a2a6217ce1ad05d37841a0c0412414b161ce0288
mrfukui <mr.fukui@yahoo.com>
2023-09-16 20:09:44 -0700

    added for loop in scripts.js to list pokemon with height

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let pokemonList = [];
+
 pokemonList = [
     {name: 'Bulbasaur',
     type: ['Grass', 'Poison'],
@@ -17,3 +18,15 @@ pokemonList = [
     height: 0.4
     }
 ];
+
+// for loop listing pokemon names and heights
+for (let i=0; i < pokemonList.length; i++) {
+
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    // condition adding a comment on the pokemon with height 0.7
+    if (pokemonList[i].height === 0.7) {
+        document.write(' - Wow, that\'s big!' + '<br>');
+    } else {
+        document.write('<br>');
+    };
+};
